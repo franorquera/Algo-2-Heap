9d71682cbaffc3ccfe6b43b53b4b8e3a463a0684
fran <franorquera88@gmail.com>
2020-12-08 23:08:14 -0300

    Primer Commit

---
+++ pruebas_heap.c
@@ -0,0 +1,20 @@
+#include "heap.h"
+#include "testing.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>  // For ssize_t in Linux.
+
+/* ******************************************************************
+ *                        PRUEBAS UNITARIAS
+ * *****************************************************************/
+
+
+
+
+/* ******************************************************************
+ *                        FUNCIÓN PRINCIPAL
+ * *****************************************************************/
+
+void pruebas_heap_estudiante();
